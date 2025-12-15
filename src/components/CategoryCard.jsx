40bbaa6fc21630b7d8db66ae58bf0c53cbd3f9e7
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-12-15 20:04:26 +0100

    Implemented static structure for "Explore categoires" section: - created reusable CategoryCard component, - added layout markup, - and set up initial styling for cards

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,7 +1,28 @@
+//css
+import "./CategoryCard.css"
+
 export function CategoryCard() {
   return (
-    <div>
-      category card
-    </div>
+    <>
+     {/* ------- CARD */}
+        <div className="cat-card">
+
+          <div className="icon-and-counter-box">
+            {/* ICON */}
+            <div className="icon">ICO</div>
+            {/* COUNTER */}
+            <div className="counter">3</div>
+          </div>
+
+          {/* TXT */}
+          <div className="cat-card-txt">
+            {/* TITLE */}
+            <h5 className="cat-title">Pianeta & natura</h5>
+            {/* DESCRIPTION */}
+            <p className="cat-description">Fenomeni naturali e planetari</p>
+          </div>
+         
+        </div>
+    </>
   );
 }
--- src/components/ExploreCategories.jsx
+++ src/components/ExploreCategories.jsx
@@ -1,7 +1,24 @@
+//components
+import { CategoryCard } from "./CategoryCard";
+
+//css
+import "./ExploreCategories.css"
+
 export function ExploreCategories() {
   return (
-    <div>
-      Explore Categories
+    <div className="explore-cat-container">
+
+      <h3>Esplora le categorie</h3>
+      <h4>Seleziona una categoria per filtrare i dati</h4>
+
+      <div className="cat-row">
+
+        {/* ------- CARD */}
+       <CategoryCard />
+
+         {/* ------- */}
+      </div>
+
     </div>
   );
 }
